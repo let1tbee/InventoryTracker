27fd79453a263efb45610ec96de71aa43f625afa
let1tbee <ratushny.aleksey@gmail.com>
2026-02-13 18:23:18 +0200

    First attempt with fastAPI

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hi")
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+items = []
+
+class Item(BaseModel):
+    name: str
+    quantity: int = 1
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items")
+def read_item():
+    return {"Items": items}
+
+@app.post("/items")
+def create_item(item: Item):
+    items.append(item)
+    return {"Items": items}
