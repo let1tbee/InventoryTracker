c277def0122efa11df9433a4f2079b4b410fef4f
let1tbee <ratushny.aleksey@gmail.com>
2026-02-18 15:57:17 +0200

    main: adding routs for Employees.

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, status
 from typing import Annotated
-from sqlmodel import create_engine, SQLModel, Field, Session
+from sqlmodel import create_engine, SQLModel, Field, Session, select
 from contextlib import asynccontextmanager
-from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
@@ -13,11 +12,18 @@ class Settings(BaseSettings):
 settings = Settings()
 engine = create_engine(settings.DB_URL, echo=True)
 
-class Hero(SQLModel, table=True):
+class EmployeesBase(SQLModel):
+    first_name: str = Field(index=True)
+    last_name: str = Field(index=True)
+    email: str | None = Field(default=None, index=True)
+
+class Employees(EmployeesBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(index=True)
-    age: int | None = Field(default=None, index=True)
-    secret_name: str
+
+class EmployeeUpdate(EmployeesBase):
+    first_name: str | None = None
+    last_name: str | None = None
+    email: str | None = None
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
@@ -45,14 +51,45 @@ app = FastAPI(title='Inventory Tracker',
 def read_root():
     return {"Welcome to Inventory Tracker API!"}
 
-@app.post("/heroes/")
-def create_hero(hero: Hero, session: SessionDep) -> Hero:
-    session.add(hero)
+@app.post("/employees/", tags = ["employees"], response_model=EmployeesBase, status_code=status.HTTP_201_CREATED)
+def create_employee(employee: EmployeesBase, session: SessionDep):
+    db_employee = Employees.model_validate(employee)
+    session.add(db_employee)
     session.commit()
-    session.refresh(hero)
-    return hero
+    session.refresh(db_employee)
+    return db_employee
 
+@app.get("/employees/{emp_id}", tags = ["employees"], response_model=EmployeesBase)
+def get_employee(emp_id: int, session: SessionDep):
+    employee = session.get(Employees, emp_id)
+    if not employee:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Employee with id {emp_id} not found")
+    return employee
 
+@app.get("/employees/", tags = ["employees"])
+def get_employees(session: SessionDep, limit: int = 10):
+    employees = session.exec(select(Employees).limit(limit)).all()
+    return employees
 
+@app.delete("/employees/{emp_id}", tags = ["employees"])
+def delete_employee(emp_id: int, session: SessionDep):
+    employee = session.get(Employees, emp_id)
+    if not employee:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Employee with id {emp_id} not found")
+    session.delete(employee)
+    session.commit()
+    return {"ok" : True}
+
+@app.patch("/employees/{emp_id}", tags = ["employees"], response_model=EmployeesBase)
+def update_employee(emp_id: int, employee: EmployeeUpdate, session: SessionDep):
+    employee_db = session.get(Employees, emp_id)
+    if not employee_db:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Employee with id {emp_id} not found")
+    employee_data = employee.model_dump(exclude_unset=True)
+    employee_db.sqlmodel_update(employee_data)
+    session.add(employee_db)
+    session.commit()
+    session.refresh(employee_db)
+    return employee_db
 
 
