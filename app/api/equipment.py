014b05eeae9380b55ad4ec3c7c2915d188729962
let1tbee <ratushny.aleksey@gmail.com>
2026-02-19 15:14:15 +0200

    api/equipment: fix.

--- app/api/equipment.py
+++ app/api/equipment.py
@@ -10,20 +10,20 @@ router = APIRouter(prefix="/equipment",
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def create_equipment(equip: models.Equipment, session: SessionDep):
-    return create_equipment(equip, session)
+    return equipment.create_equipment(equip, session)
 
 @router.get("/{equip_id}")
 def get_equipment(equip_id: int, session: SessionDep):
-    return get_equipment(equip_id, session)
+    return equipment.get_equipment(equip_id, session)
 
 @router.get("/")
 def get_equipments(session: SessionDep, offset: int = 0, limit: int = 10):
-    return get_equipments(session, offset, limit)
+    return equipment.get_equipments(session, offset, limit)
 
 @router.delete("/{emp_id}")
 def delete_equipment(emp_id: int, session: SessionDep):
-    return delete_equipment(emp_id, session)
+    return equipment.delete_equipment(emp_id, session)
 
 @router.patch("/{emp_id}")
 def update_equipment(emp_id: int, equip: models.EquipmentUpdate, session: SessionDep):
-    return update_equipment(emp_id, equip, session)
+    return equipment.update_equipment(emp_id, equip, session)
