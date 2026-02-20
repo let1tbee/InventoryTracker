3f8c94566bb054d34103a6a22bc03e70c0857a6a
let1tbee <ratushny.aleksey@gmail.com>
2026-02-20 13:00:56 +0200

    test_employees.py: changed test logic to create dummy DB for every test. Wrapped test into a Clas.

--- conftest.py
+++ conftest.py
@@ -4,25 +4,25 @@ from app.database import get_session
 from main import app
 import pytest
 
-@pytest.fixture(scope ="module")
+@pytest.fixture()
 def engine():
     sqlite_url = f"sqlite:///:memory:"
     connect_args = {"check_same_thread": False}
     engine = create_engine(sqlite_url, connect_args=connect_args, poolclass=StaticPool)
     return engine
 
-@pytest.fixture(scope ="module")
+@pytest.fixture()
 def setup_tables(engine):
     SQLModel.metadata.create_all(engine)
     yield
     SQLModel.metadata.drop_all(engine)
 
-@pytest.fixture(scope ="module")
+@pytest.fixture()
 def setup_session(engine, setup_tables):
     with Session(engine) as session:
         yield session
 
-@pytest.fixture(scope ="module")
+@pytest.fixture()
 def client(setup_session):
     def override_get_session():
         return setup_session
--- tests/test_employees.py
+++ tests/test_employees.py
@@ -1,4 +1,6 @@
 import pytest
+from app import models
+from app.models import ItemStatus
 
 test_employees = [{
         "first_name": "test_first_name",
@@ -16,71 +18,87 @@ test_employees = [{
         "email": "test2@test",
         }]
 
-
-@pytest.fixture
-def create_assigned_equipment(client):
-    client.post("/equipment/", json={
+test_equipment = [{
         "name": "laptop",
         "s_n": "0001"
-        })
-    client.patch("/equipment/assign/1?assignee_id=1")
+        }]
 
-@pytest.mark.parametrize("employee", test_employees)
-def test_create_employee(employee, client):
-    response = client.post("/employees/", json=employee)
-    assert response.status_code == 201
+@pytest.fixture()
+def fill_test_tables(setup_session):
+    for i in test_employees:
+        empl_db = models.Employees.model_validate(i)
+        setup_session.add(empl_db)
 
+    for i in test_equipment:
+        equip_db = models.Equipment.model_validate(i)
+        empl_db = setup_session.get(models.Employees, 1)
+        setup_session.add(equip_db)
+        equip_db.assignee = empl_db
+        equip_db.status = ItemStatus.ASSIGNED
 
-def test_get_employees(client):
-    response = client.get("/employees/")
-    assert response.status_code == 200
-    data = response.json()
-    assert len(data) == len(test_employees)
-    for i in range(len(test_employees)):
-        assert data[i]["first_name"] == test_employees[i]["first_name"]
-        assert data[i]["last_name"] == test_employees[i]["last_name"]
-        assert data[i]["email"] == test_employees[i]["email"]
+    setup_session.commit()
 
-def test_get_employee(client):
-    response = client.get("/employees/1")
-    assert response.status_code == 200
-    data = response.json()
-    assert data["first_name"] == test_employees[0]["first_name"]
-    assert data["last_name"] == test_employees[0]["last_name"]
-    assert data["email"] == test_employees[0]["email"]
+@pytest.mark.usefixtures("fill_test_tables")
+class TestEmployees:
 
-@pytest.mark.parametrize("employee", test_employees)
-def test_search_equipments(employee,client):
-    link = "/employees/search/?first_name="+employee["first_name"]+"&last_name="+employee["last_name"]+"&email="+employee["email"]
-    response = client.get(link)
-    assert response.status_code == 200
-    data = response.json()
-    assert data[0]["first_name"] == employee["first_name"]
-    assert data[0]["last_name"] == employee["last_name"]
-    assert data[0]["email"] == employee["email"]
+    def test_create_employee(self, client):
+        response = client.post("/employees/", json={
+            "first_name": "test_first_name3",
+            "last_name": "test_last_name3",
+            "email": "test3@test",
+            })
+        assert response.status_code == 201
 
-def test_get_assigned_equipment(create_assigned_equipment,client):
-    response = client.get("/employees/equipment/1")
-    assert response.status_code == 200
-    data = response.json()
-    print(data)
-    assert data
+    def test_get_employees(self, client):
+        response = client.get("/employees/")
+        assert response.status_code == 200
+        data = response.json()
+        assert len(data) == len(test_employees)
+        for i in range(len(test_employees)):
+            assert data[i]["first_name"] == test_employees[i]["first_name"]
+            assert data[i]["last_name"] == test_employees[i]["last_name"]
+            assert data[i]["email"] == test_employees[i]["email"]
 
-def test_update_employee(client):
-    response = client.patch("/employees/1", json={"first_name" : "changed_first_name"})
-    assert response.status_code == 200
-    data = response.json()
-    assert data["first_name"] == "changed_first_name"
+    def test_get_employee(self, client):
+        response = client.get("/employees/1")
+        assert response.status_code == 200
+        data = response.json()
+        assert data["first_name"] == test_employees[0]["first_name"]
+        assert data["last_name"] == test_employees[0]["last_name"]
+        assert data["email"] == test_employees[0]["email"]
 
-def test_delete_employee(client):
-    response = client.delete("/employees/1")
-    assert response.status_code == 200
-    data = response.json()
-    assert data == {'ok': True}
-    response_del = client.get("/employees/")
-    assert response_del.status_code == 200
-    data_del = response_del.json()
-    assert len(data_del) == len(test_employees) - 1
+    @pytest.mark.parametrize("employee", test_employees)
+    def test_search_equipments(self, employee, client):
+        link = "/employees/search/?first_name="+employee["first_name"]+"&last_name="+employee["last_name"]+"&email="+employee["email"]
+        response = client.get(link)
+        assert response.status_code == 200
+        data = response.json()
+        assert data[0]["first_name"] == employee["first_name"]
+        assert data[0]["last_name"] == employee["last_name"]
+        assert data[0]["email"] == employee["email"]
+
+    def test_get_assigned_equipment(self, client):
+        response = client.get("/employees/equipment/1")
+        assert response.status_code == 200
+        data = response.json()
+        print(data)
+        assert data
+
+    def test_update_employee(self, client):
+        response = client.patch("/employees/1", json={"first_name" : "changed_first_name"})
+        assert response.status_code == 200
+        data = response.json()
+        assert data["first_name"] == "changed_first_name"
+
+    def test_delete_employee(self, client):
+        response = client.delete("/employees/1")
+        assert response.status_code == 200
+        data = response.json()
+        assert data == {'ok': True}
+        response_del = client.get("/employees/")
+        assert response_del.status_code == 200
+        data_del = response_del.json()
+        assert len(data_del) == len(test_employees) - 1
 
 
 
